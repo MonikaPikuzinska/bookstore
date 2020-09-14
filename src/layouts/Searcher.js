259c454dda00f61862fd466467345b90d7ae3c79
unknown <monika.pikuzinska01@gmai.com>
2020-09-14 12:20:40 +0200

    updated searcher

--- src/layouts/Searcher.js
+++ src/layouts/Searcher.js
@@ -1,12 +1,60 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import search from '../img/search.svg';
+import close from '../img/close.svg';
+import { NavLink } from 'react-router-dom';
 
 function Searcher() {
   const [books, setBooks ] = useState([]);
 
 
-  let API = ``;
+  let API = ``, nr = 0;
+
+  const handleDeleteDivs = () => {
+    let divs = document.querySelectorAll('.searcher__form__search');
+    nr = 0;
+    divs.forEach(div => {
+        div.remove()
+      })
+  }
+  
+  const handleShowSearch = (r) => {
+    let form = document.querySelector('.searcher__form');
+    if (!document.querySelector('.searcher__form__btn')) {
+      let button = document.createElement('img');
+      button.onclick = handleDeleteDivs;
+      button.setAttribute('class', `searcher__form__search searcher__form__btn`);
+      button.setAttribute('src', `${close}`);
+      form.appendChild(button)
+      };
+    if (r.length === 0) {
+      let div = document.createElement('div');
+      div.innerHTML = "There is no book you are looking for";
+      div.setAttribute('class', `searcher__form__search searcher__form__output`);
+      form.appendChild(div);
+    } else {
+      r.forEach((book) => {
+        let div2 = document.createElement('div');
+        div2.innerHTML = `${book.volumeInfo.authors} - ${book.volumeInfo.title}`;
+        div2.setAttribute('class', `searcher__form__search searcher__form__output searcher__form__output${nr}`);
+        form.appendChild(div2);
+        nr++;
+      })
+    }
+  }
+
+  const handleGetTitle =  () => {
+    axios.get(API)
+    .then(function (response) {
+        console.log(response.data);
+        setBooks(response.data.items);
+        return response.data;
+    } )
+    .then(function () {
+      if(books.length!==0){
+        handleShowSearch(books);
+      }
+    } )
+  };
 
   const handleSearch = () => {
     let text = document.getElementsByClassName('searcher__form__input').value
@@ -17,15 +65,13 @@ function Searcher() {
       handleGetTitle()
     }
   }
-  const handleGetTitle = async () => {
-    const response = await axios.get(API);
-    setBooks(response.data.items);
-  };
+
+
+
   return (
     <div className="searcher">
       <form className="searcher__form">
-        <input type="text" className="searcher__form__input" placeholder="Find a book"/>
-        <img src={search} className="searcher__form__btn"alt="search button" onClick={handleSearch}/>
+        <input type="text" className="searcher__form__input" placeholder="Find a book" onChange={handleSearch}/>
       </form>
     </div>
   );

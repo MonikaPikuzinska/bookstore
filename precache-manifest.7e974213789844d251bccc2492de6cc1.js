aa375765a9f5fbdd64ff78440e9a5afb903a19e9
unknown <monika.pikuzinska01@gmai.com>
2020-10-02 10:04:44 +0200

    Updates

---
+++ precache-manifest.7e974213789844d251bccc2492de6cc1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91d581b88afb05062d0d4478625ebd9b",
+    "url": "/bookstore/index.html"
+  },
+  {
+    "revision": "8a99e282d43a646cd364",
+    "url": "/bookstore/static/css/main.06f07917.chunk.css"
+  },
+  {
+    "revision": "149233b07f47f19b0e01",
+    "url": "/bookstore/static/js/2.dd6e6edb.chunk.js"
+  },
+  {
+    "revision": "8bc88370bc2489d86f2f357f447ddb0b",
+    "url": "/bookstore/static/js/2.dd6e6edb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a99e282d43a646cd364",
+    "url": "/bookstore/static/js/main.b29030f5.chunk.js"
+  },
+  {
+    "revision": "01dfa390d09b3883de91",
+    "url": "/bookstore/static/js/runtime-main.2bf98abd.js"
+  },
+  {
+    "revision": "ad45da59ee227340baa8e14daec8ed5d",
+    "url": "/bookstore/static/media/basket.ad45da59.svg"
+  },
+  {
+    "revision": "f421c9f64793ca7554d2e2fd16bb1728",
+    "url": "/bookstore/static/media/burger.f421c9f6.svg"
+  },
+  {
+    "revision": "b135172cd246b6e3b27af04827ae8bd7",
+    "url": "/bookstore/static/media/close.b135172c.svg"
+  },
+  {
+    "revision": "373569e0b273d36281ed50dcb5891428",
+    "url": "/bookstore/static/media/facebook.373569e0.svg"
+  },
+  {
+    "revision": "55ca42868146fc709817537f8a766d97",
+    "url": "/bookstore/static/media/instagram.55ca4286.svg"
+  },
+  {
+    "revision": "e8705c2b2ae5b2fda622ba882783e187",
+    "url": "/bookstore/static/media/logo.e8705c2b.svg"
+  },
+  {
+    "revision": "2d61e0a8f8d17012a0b05e33a712ca82",
+    "url": "/bookstore/static/media/twitter.2d61e0a8.svg"
+  }
+]);

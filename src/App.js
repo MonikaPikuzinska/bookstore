281020190fe9ac9baf2e1e27c46ea01d529e2847
unknown <monika.pikuzinska01@gmai.com>
2020-10-02 09:53:42 +0200

    tired to fix routing

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import Header from './layouts/Header';
 import Page from './layouts/Page';
 import Footer from './layouts/Footer';
 import './css/main.min.css';
-
+import { BrowserRouter as Router } from 'react-router-dom'; 
 
 function App() {
   return (
+    <Router basename={process.env.PUBLIC_URL}>
     <div className="app">
       <Header/>
       <Page/>
       <Footer/>
     </div>
+    </Router>
   );
 }
 
